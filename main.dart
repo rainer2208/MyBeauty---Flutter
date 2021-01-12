b8f9ab4b1dac1b92b13d0160a7e048ba9ba2542d
rainer2208 <rainer.uwe@hotmail.com>
2021-01-12 08:24:13 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -108,7 +108,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
